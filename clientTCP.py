5b75a415af745d5c74c2eb8e2c684e19ea945a1d
Adriano Freitas <archl0rd@adrianofreitas.me>
2017-03-19 16:44:04 +0000

    updated 170319

--- clientTCP.py
+++ clientTCP.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 #
 # Adriano Freitas <https://adrianofreitas.me>
 # Robson Marques <http://rbmarques.com.br>
@@ -6,12 +7,14 @@
 import socket
 HOST = '127.0.0.1'
 PORT = 1337
+print 'Trabalho para a disciplina Programação de Sistemas Distribuídos'
+print 'Adriano Freitas <https://adrianofreitas.me>'
+print 'Robson Marques  <http://rbmarques.com.br>'
+print '\nPara sair use CTRL+X\n'
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
-print '\n'
-print 'Trabalho para a disciplina Programação de Sistemas Distribuídos'
-print 'Para sair use CTRL+X\n'
 msg = raw_input()
 while msg <> '\x18':
     tcp.send (msg)
--- serverTCP.py
+++ serverTCP.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 #
 # Adriano Freitas <https://adrianofreitas.me>
 # Robson Marques <http://rbmarques.com.br>
@@ -6,16 +7,23 @@
 import socket
 HOST = ''
 PORT = 1337
+
+def menu():
+    print '1 - Listar recursos existentes'
+    print '2 - Status recurso'
+    print '3 - Reservar recurso'
+    print '4 - Liberar recurso'
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
 while True:
     con, cliente = tcp.accept()
-    print 'Concetado por', cliente
+    print 'Cliente conectado', cliente
     while True:
         msg = con.recv(1024)
         if not msg: break
         print cliente, msg
-    print 'Finalizando conexao do cliente', cliente
+    print 'Finalizando conexao..', cliente
     con.close()
